8ff0a495abfd1c57493ad11a90ada22ad80edc38
AilPkk <ailar.pokk@gmail.com>
2024-09-07 14:30:35 +0300

    Initial Commit Program structure

---
+++ main.py
@@ -0,0 +1,22 @@
+# This script reads text from statistics and creates paragraphs of text
+
+def read_xlsx():
+    # reads and gived data coherent structure
+    filename = "Geoloogiline lõige koos statistikaga.xlsx"
+
+"""Key lines:
+A: Puuraugu number			
+D: Teepinnast sügavus (m)
+D: Abs (m)
+D: Kaetud kihi nr
+D: Kiht esines puuraukudes 
+"""
+
+def write_text(table):
+    # Creates file and fills it with necessary info
+    pass
+
+if __name__ == '__main__':
+    # read_xlsx
+    # write_text
+    pass
