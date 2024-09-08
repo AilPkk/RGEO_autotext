550ec757f9dcad46316cf3eb98fab477b1b15276
AilPkk <ailar.pokk@gmail.com>
2024-09-08 18:54:41 +0300

    Organize dataf

--- main.py
+++ main.py
@@ -21,15 +21,23 @@ def read_xlsx(file, dataf):
     return dataf_filtered
 
 def organize_data(dataf_filtered):
-    pass
+    # Data will be sorted into respective tables
 
-"""Key lines:
-A: Puuraugu number			
-D: Teepinnast sügavus (m)
-D: Abs (m)
-D: Kaetud kihi nr
-D: Kiht esines puuraukudes 
-"""
+    split_index = [0]
+    table_headers = ["Teepinnast sügavus (m)", "Abs (m)", "Kaetud kihi nr", "Kiht esines puuraukudes "]
+
+    for i in range (len(dataf_filtered)-1):
+        try:
+            if dataf_filtered[i][3] in table_headers:
+                split_index.append(i)
+        except: pass
+
+    paksus_m = dataf_filtered[:split_index[1]]
+    sygavus_m = dataf_filtered[split_index[1]:split_index[2]]
+    abs_m =  dataf_filtered[split_index[2]:split_index[3]]
+    kaetud_nr =  dataf_filtered[split_index[3]:split_index[4]]
+    esines_nr =  dataf_filtered[split_index[4]:]
+
 
 def write_text(table):
     # Creates file and fills it with necessary info
@@ -40,13 +48,13 @@ dataframe = []  # declare empty df
 
 if __name__ == '__main__':
     dataframe_filtered = read_xlsx(filename, dataframe)
-    # organize_data(dataframe_filtered)
+    organize_data(dataframe_filtered)
     # write_text
     pass
 
 # debug sh1t
-csv_path = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\test.csv"
-with open(csv_path, "w", newline="") as f:
-    write = csv.writer(f)
-    write.writerows(dataframe_filtered)
+# csv_path = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\test.csv"
+# with open(csv_path, "w", newline="") as f:
+#     write = csv.writer(f)
+#     write.writerows(dataframe_filtered)
 
