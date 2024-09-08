5791f5182420b29ffc5f24ff256f2db26fb99a0f
AilPkk <ailar.pokk@gmail.com>
2024-09-08 13:02:51 +0300

    Omit empty lines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This script reads text from statistics and creates paragraphs of text
 import openpyxl as opx
+import csv
 
 
 def read_xlsx(file):
@@ -13,19 +14,34 @@ def read_xlsx(file):
 
     print(max_row, max_col)
 
-    dataf = [[] for _ in range(max_col + 1)]
+    dataf = []
+    dataf = [[] for _ in range(max_row + 1)]
 
     for i in range (1, max_row + 1):
         print(round(i / max_row * 100, 1))
-        if sheet.cell(row = i, column = 4).value is not None:
+        if sheet.cell(row = i, column = 1).value is not None\
+                or sheet.cell(row = i, column = 4).value is not None:
+#        if True:
 
-            for j in range (1, max_col + 1):
-                try:
-                    dataf[i].append(sheet.cell(row = i, column = j).value)
-                except:
-                    pass
+            try:
+                for j in range (1, max_col + 1):
+                    cell_val = sheet.cell(row = i, column = j).value
+                    if cell_val is None:
+                        dataf[i].append("")
+                    else:
+                        dataf[i].append(cell_val)
+            except:
+                pass
 
-    print(dataf)
+    dataf = [x for x in dataf if len(x)>0]
+
+
+# debug sh1t
+    csv_path = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\test.csv"
+    with open(csv_path, "w", newline="") as f:
+        write = csv.writer(f)
+        write.writerows(dataf)
+
 
 
 
@@ -47,3 +63,4 @@ if __name__ == '__main__':
     read_xlsx(filename)
     # write_text
     pass
+
