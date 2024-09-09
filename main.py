5436d62a3ffca12027cebfd9829ac212a37e9bd4
AilPkk <ailar.pokk@gmail.com>
2024-09-09 08:52:54 +0300

    Omit main, its a single pass program anyway

--- main.py
+++ main.py
@@ -2,55 +2,51 @@
 import openpyxl as opx
 import csv
 
+xls_name = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\Geoloogiline lõige koos statistikaga.xlsx"
+dataframe = []  # declare empty df
 
-def read_xlsx(file, dataf):
-    # reads data
 
-    workbook = opx.load_workbook(file, read_only=True, data_only=True)
-    sheet = workbook.active
+# reads data
 
-    for row in sheet.values:
-        dataf.append(list(row))
+workbook = opx.load_workbook(xls_name, read_only=True, data_only=True)
+sheet = workbook.active
 
-    # list cleanup
-    dataf_filtered = [x for x in dataf if x[3] is not None or x[1] is not None]
-    for line in dataf_filtered:
-        while line[-1] is None:
-            del line[-1]
+for row in sheet.values:
+    dataframe.append(list(row))
 
-    return dataf_filtered
+workbook.close()
 
-def organize_data(dataf_filtered):
-    # Data will be sorted into respective tables
+# cleanup
+dataframe_filtered = [x for x in dataframe if x[3] is not None or x[1] is not None]
+for line in dataframe_filtered:
+    while line[-1] is None:
+        del line[-1]
 
-    split_index = [0]
-    table_headers = ["Teepinnast sügavus (m)", "Abs (m)", "Kaetud kihi nr", "Kiht esines puuraukudes "]
 
-    for i in range (len(dataf_filtered)-1):
-        try:
-            if dataf_filtered[i][3] in table_headers:
-                split_index.append(i)
-        except: pass
+# Data will be sorted into respective tables
 
-    paksus_m = dataf_filtered[:split_index[1]]
-    sygavus_m = dataf_filtered[split_index[1]:split_index[2]]
-    abs_m =  dataf_filtered[split_index[2]:split_index[3]]
-    kaetud_nr =  dataf_filtered[split_index[3]:split_index[4]]
-    esines_nr =  dataf_filtered[split_index[4]:]
+split_index = [0]
+table_headers = ["Teepinnast sügavus (m)", "Abs (m)", "Kaetud kihi nr", "Kiht esines puuraukudes "]
 
+for i in range (len(dataframe_filtered)-1):
+    try:
+        if dataframe_filtered[i][3] in table_headers:
+            split_index.append(i)
+    except: pass
+
+paksus_m = dataframe_filtered[:split_index[1]]
+sygavus_m = dataframe_filtered[split_index[1]:split_index[2]]
+abs_m =  dataframe_filtered[split_index[2]:split_index[3]]
+kaetud_nr =  dataframe_filtered[split_index[3]:split_index[4]]
+esines_nr =  dataframe_filtered[split_index[4]:]
+
+#    print(len(paksus_m), len(sygavus_m), len(abs_m), len(kaetud_nr), len(esines_nr))
+#    print(kaetud_nr)
 
 def write_text(table):
     # Creates file and fills it with necessary info
     pass
 
-filename = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\Geoloogiline lõige koos statistikaga.xlsx"
-dataframe = []  # declare empty df
-
-if __name__ == '__main__':
-    dataframe_filtered = read_xlsx(filename, dataframe)
-    organize_data(dataframe_filtered)
-    # write_text
-    pass
 
 # debug sh1t
 # csv_path = "C:\\Users\\saara\\OneDrive\\Töölaud\\Rakendusgeoloogia\\Ailar\\py\\autotext\\sample\\test.csv"
